7c9737ac3ed69681634c5e6fafa40fefcd6f6b00
shary144 <kouki.6318@gmail.com>
2026-02-06 10:31:30 +0900

    ファイル名やディレクトリ名で、先頭に数字を置くことはあまりよくないようですね。ということで名前を変えようと思います。よろしくお願いします。

--- src/cvpkg/setup.py
+++ src/cvpkg/setup.py
@@ -6,9 +6,6 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-
-    package_data={package_name: ['*.webp', '*.png']}, 
-    
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
